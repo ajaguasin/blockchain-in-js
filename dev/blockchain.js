12cf19ee1ed5a7ffe1bbfd12855de74323421da2
aj <ajaguasin@gmail.com>
2019-09-22 16:16:56 -0700

    Create typedef for Block, create getLastBlock method

--- dev/blockchain.js
+++ dev/blockchain.js
@@ -1,3 +1,13 @@
+/**
+ * @typedef {object} Block
+ * @property {number} index The index
+ * @property {Date} timestamp The Date of when the block was created
+ * @property {Block[]} transactions Instance's array of new transactions
+ * @property {number} nonce Proof of work
+ * @property {string} hash Data from the new block hashed into a string
+ * @property {string} previousBlockHash Dta from the new block hashed into a string
+ */
+
 class Blockchain {
 	constructor() {
 		this.chain = [];
@@ -9,7 +19,7 @@ class Blockchain {
 	 * @param {number} nonce Proof of work
 	 * @param {string} previousBlockHash Data from the previous block hashed into a string
 	 * @param {string} hash Data from the new block hashed into a string
-	 * @returns {object} block object
+	 * @returns {Block} block object
 	 */
 	createNewBlock(nonce, previousBlockHash, hash) {
 		const newBlock = {
@@ -26,4 +36,13 @@ class Blockchain {
 
 		return newBlock;
 	}
+
+	/**
+	 * @returns {Block} The last block of the chain
+	 */
+	getLastBlock() {
+		return this.chain[this.chain.length - 1];
+	}
 }
+
+module.exports = Blockchain;
